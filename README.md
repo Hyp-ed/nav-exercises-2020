eb2d760a31f8bdda33362ae83a0ec1b31fbbd4ac
Justus Rudolph <justus.katte@gmail.com>
2019-10-24 23:17:56 +0100

    Update kalman filter class with methods and member variables we added today. Also add a simple Kalman Filtering cheat sheet.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # nav-exercises-2020
 
 This repo is used for navigation exercises in the beginning of the year 19/20.
+
--- src/navigation/kalman_filter.hpp
+++ src/navigation/kalman_filter.hpp
@@ -3,6 +3,46 @@
 #include <iostream>
 #include <Eigen/Dense>
 
+using Eigen::VectorXf;
+using Eigen::MatrixXf;
 
+class KalmanFilter {
+  public:
+    /*
+     * Constructor call for a kalman filter
+     * n : dimension of the state
+     * m : dimension of the measurement
+     * k : dimension of control   NOTE: will not be used in this implementation
+     */
+    KalmanFilter(uint8_t n, uint8_t m, uint8_t k = 0);
 
-#endif
+    // f6 : set_initial
+    // f7 : filter : Calls five main kalman filtering equations / functions
+    // f8 : get_state
+    // f9 : get_covariance
+
+  private:
+    // f1 : predict state
+    // f2 : estimate state (update based on measurement)
+    // f3 : kalman gain
+    // f4 : predict state covariance
+    // f5 : estimate state covariance
+    // f10 : update state transition
+    static constexpr float kStateCovarianceNoise = 0.01;
+    static constexpr float kMeasurementNoise = 0.01;
+
+    uint8_t n_;
+    uint8_t m_;
+    uint8_t k_;
+
+    VectorXf x_;  // current state (n x 1)
+    VectorXf z_;  // measurement (m x 1)
+    MatrixXf A_;  // state transition matrix (n x n)
+    MatrixXf P_;  // state covariance matrix (n x n)
+    MatrixXf Q_;  // state covariance noise matrix (n x n)
+    MatrixXf H_;  // dimension change matrix (m x n)
+    MatrixXf R_;  // measurement noise matrix (m x m)
+    MatrixXf I_;  // identity matrix (n x n)
+
+};
+#endif  // KALMAN_FILTER_HPP
