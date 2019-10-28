e9a5efe24f7fd843029cf8535c64838efe97a360
alexchelba <chelba_alexandru@yahoo.com>
2019-10-28 19:46:34 +0000

    Basic Kalman Filter without initializations

---
+++ src/navigation/kalman_filter.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include<kalman_filter.hpp>
+
+KalmanFilter(uint8_t n, uint8_t m, uint8_t k) {
+	n_ = n;
+	m_ = n;
+	k_ = k;
+}
+VectorXf KalmanFilter::set_initial(float pos, float velocity, float acc) {
+	return VectorXf({ 0,0,0 });
+}
+VectorXf KalmanFilter::get_state() {
+	return x_;
+}
+MatrixXf KalmanFilter::get_covariance() {
+	return P_;
+}
+VectorXf KalmanFilter::predict_state() {
+	VectorXf x;
+	x = A_ * x_;// + B_ * z_;
+	return x;
+}
+MatrixXf KalmanFilter::predict_covariance() {
+	MatrixXf P;
+	P = A_ * P_ * A_.transpose() + Q_;
+	return P;
+}
+MatrixXf KalmanFilter::kalman_gain() {
+	MatrixXf Kn;
+	Kn = H_ * P_ * H_.transpose() * ((H_ * P_ * H_.transpose()) + R_).inverse();
+	return Kn;
+}
+VectorXf KalmanFilter::estimate_state() {
+	VectorXf x;
+	x = x_ + (Kn_ * (z_ - (H_ * x_)));
+	return x;
+}
+MatrixXf KalmanFilter::estimate_state_covariance() {
+	MatrixXf P;
+	P = (I_ - (Kn_ * H_)) * P_;
+	return P;
+}
+void KalmanFilter::call_equations() {
+	x_ = set_initial();
+	x_ = predict_state();
+	P_ = predict_state_covariance();
+	Kn_ = kalman_gain();
+	x_ = estimate_state();
+	P_ = estimate_state_covariance();
+	update_state_transition();
+}
+int main() {
+	return 0;
+}
--- src/navigation/kalman_filter.hpp
+++ src/navigation/kalman_filter.hpp
@@ -15,19 +15,33 @@ class KalmanFilter {
      * k : dimension of control   NOTE: will not be used in this implementation
      */
     KalmanFilter(uint8_t n, uint8_t m, uint8_t k = 0);
-
+	
     // f6 : set_initial
+	VectorXf set_initial(float pos, float velocity, float acc);
     // f7 : filter : Calls five main kalman filtering equations / functions
+	void call_equations();
     // f8 : get_state
+	VectorXf get_state();
     // f9 : get_covariance
+	MatrixXf get_covariance();
 
   private:
     // f1 : predict state
+	  VectorXf predict_state();
+	// f4 : predict state covariance
+	  MatrixXf predict_state_covariance();
+
+	// f3 : kalman gain
+	  VectorXf kalman_gain();
+
     // f2 : estimate state (update based on measurement)
-    // f3 : kalman gain
-    // f4 : predict state covariance
-    // f5 : estimate state covariance
+	  VectorXf estimate_state();
+	// f5 : estimate state covariance
+	  MatrixXf estimate_state_covariance();
+
     // f10 : update state transition
+	  MatrixXf update_state_transition();
+
     static constexpr float kStateCovarianceNoise = 0.01;
     static constexpr float kMeasurementNoise = 0.01;
 
@@ -38,11 +52,13 @@ class KalmanFilter {
     VectorXf x_;  // current state (n x 1)
     VectorXf z_;  // measurement (m x 1)
     MatrixXf A_;  // state transition matrix (n x n)
+	MatrixXf B_;  // matrix that transforms my input into usable data
     MatrixXf P_;  // state covariance matrix (n x n)
     MatrixXf Q_;  // state covariance noise matrix (n x n)
     MatrixXf H_;  // dimension change matrix (m x n)
     MatrixXf R_;  // measurement noise matrix (m x m)
     MatrixXf I_;  // identity matrix (n x n)
+	MatrixXf Kn_; // Kalman gain
 
 };
 #endif  // KALMAN_FILTER_HPP
